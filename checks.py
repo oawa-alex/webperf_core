05682649d2a701458bb0015a1c977de19c9ef738
Marcus Österberg <m@tba.nu>
2019-12-31 15:35:15 +0100

    First edition of standard files check

--- checks.py
+++ checks.py
@@ -27,7 +27,7 @@ def check_four_o_four(url):
     ## kollar koden
     o = urllib.parse.urlparse(url)
     url = '{0}://{1}/{2}-{3}.html'.format(o.scheme, o.netloc, 's1d4-f1nns-1nt3', get_guid(5))
-    headers = {'user-agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
+    headers = {'user-agent': config.useragent}
     request = requests.get(url, allow_redirects=False, headers=headers, timeout=request_timeout)
     code = request.status_code
     if code == 404:
@@ -376,6 +376,72 @@ def check_privacy_webbkollen(url):
 
         return (points, review, return_dict)
 
+def standard_files(url):
+    """
+    Looking for:
+    * robots.txt
+    * at least one sitemap/siteindex mentioned in robots.txt
+    * a RSS feed mentioned in the page's meta
+    """
+    o = urllib.parse.urlparse(url)
+    parsed_url = '{0}://{1}/'.format(o.scheme, o.netloc)
+    robots_content = httpRequestGetContent(parsed_url + 'robots.txt')
+
+    review = ''
+    return_dict = dict()
+    points = 5.0
+    if 'user-agent' not in robots_content.lower() and 'disallow' not in robots_content.lower() and 'allow' not in robots_content.lower():
+        points -= 3
+        review += '* robots.txt saknas eller har inte förväntat innehåll.\n'
+        return_dict['robots.txt'] = 'missing content'
+    else:
+        review += '* robots.txt verkar ok.\n'
+        return_dict['robots.txt'] = 'ok'
+    if 'sitemap:' not in robots_content.lower():
+        points -= 2
+        review += '* Sitemap anges inte i robots.txt\n'
+        return_dict['sitemap'] = 'not in robots.txt'
+    else:
+        review += '* Sitemap finns omnämnd i robots.txt\n'
+        return_dict['sitemap'] = 'ok'
+
+    smap_pos = robots_content.lower().find('sitemap')
+    smaps = robots_content[smap_pos:].split('\n')
+    found_smaps = []
+    for line in smaps:
+        if 'sitemap:' in line.lower():
+            found_smaps.append(line.lower().replace('sitemap:', '').strip())
+    return_dict["num_sitemaps"] = len(found_smaps)
+    if len(found_smaps) > 0:
+        return_dict["sitemaps"] = found_smaps
+    
+    # TODO: validate the first sitemap
+    
+    # TODO: validate first feed
+    headers = {'user-agent': config.useragent}
+    request = requests.get(url, allow_redirects=True, headers=headers, timeout=request_timeout)
+
+    soup = BeautifulSoup(request.text, 'lxml')
+    feed = soup.find_all(rel='alternate')
+
+    if len(feed) is 0:
+        points -= 1
+        review += '* RSS-prenumeration saknas i meta.\n'
+        return_dict['feed'] = 'not in meta'
+        return_dict['num_feeds'] = len(feed)
+    elif len(feed) > 0:
+        review += '* RSS-prenumeration hittad.\n'
+        return_dict['feed'] = 'found in meta'
+        return_dict['num_feeds'] = len(feed)
+        tmp_feed = []
+        for single_feed in feed:
+            tmp_feed.append(single_feed.get('href'))
+
+        return_dict['feeds'] = tmp_feed
+
+    return (points, review, return_dict)
+
+
 def httpRequestGetContent(url):
     """Trying to fetch the response content
     Attributes: url, as for the URL to fetch
@@ -404,8 +470,17 @@ def get_guid(length):
     """
     return str(uuid.uuid4())[0:length]
 
+def is_sitemap(content):
+    """Check a string to see if its content is a sitemap or siteindex.
+
+    Attributes: content (string)
+    """
+    if 'http://www.sitemaps.org/schemas/sitemap/' in content or '<sitemapindex' in content: 
+        return True
+
+    return False
 """
 If file is executed on itself then call a definition, mostly for testing purposes
 """
 if __name__ == '__main__':
-    print(check_google_pagespeed('https://webperf.se'))
+    print(standard_files('https://webperf.se'))
