9f705bac141b7ee6d63cf8016bf4de0785226175
Mattias <github-4c85@minadress.nu>
2021-04-07 21:03:29 +0200

    test of running test locally

--- tests/http_validator.py
+++ tests/http_validator.py
@@ -24,6 +24,33 @@ from urllib.parse import urlparse
 import uuid
 import re
 from bs4 import BeautifulSoup
+
+import argparse
+import asyncio
+import logging
+import os
+import pickle
+import ssl
+import time
+from collections import deque
+from typing import BinaryIO, Callable, Deque, Dict, List, Optional, Union, cast
+from urllib.parse import urlparse
+
+import aioquic
+from aioquic.asyncio.client import connect
+from aioquic.asyncio.protocol import QuicConnectionProtocol
+from aioquic.h0.connection import H0_ALPN, H0Connection
+from aioquic.h3.connection import H3_ALPN, H3Connection
+from aioquic.h3.events import (
+    DataReceived,
+    H3Event,
+    HeadersReceived,
+    PushPromiseReceived,
+)
+from aioquic.quic.configuration import QuicConfiguration
+from aioquic.quic.events import QuicEvent
+from aioquic.tls import CipherSuite, SessionTicket
+
 import config
 from tests.utils import httpRequestGetContent, has_redirect
 import gettext
@@ -356,6 +383,11 @@ def http_version_score(hostname, url, _):
         points += 0.5
         review += _('TEXT_REVIEW_HTTP_VERSION_HTTP_2')
 
+    result = check_http3(url)
+    if result[0]:
+        points += 0.5
+        review += _('TEXT_REVIEW_HTTP_VERSION_HTTP_3')
+
     # If we still have 0.0 points something must have gone wrong, try fallback
     if points == 0.0:
         result = check_http_fallback(url)
@@ -418,6 +450,211 @@ def check_http2(hostname):
             return (False, "http2")
     except Exception:
         return (False, "http2")
+
+
+logger = logging.getLogger("client")
+
+HttpConnection = Union[H0Connection, H3Connection]
+
+USER_AGENT = "aioquic/" + aioquic.__version__
+
+
+class URL:
+    def __init__(self, url: str) -> None:
+        parsed = urlparse(url)
+
+        self.authority = parsed.netloc
+        self.full_path = parsed.path
+        if parsed.query:
+            self.full_path += "?" + parsed.query
+        self.scheme = parsed.scheme
+
+
+class HttpRequest:
+    def __init__(
+        self, method: str, url: URL, content: bytes = b"", headers: Dict = {}
+    ) -> None:
+        self.content = content
+        self.headers = headers
+        self.method = method
+        self.url = url
+
+
+class HttpClient(QuicConnectionProtocol):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.pushes: Dict[int, Deque[H3Event]] = {}
+        self._http: Optional[HttpConnection] = None
+        self._request_events: Dict[int, Deque[H3Event]] = {}
+        self._request_waiter: Dict[int, asyncio.Future[Deque[H3Event]]] = {}
+
+        # if self._quic.configuration.alpn_protocols[0].startswith("hq-"):
+        #    self._http = H0Connection(self._quic)
+        # else:
+        self._http = H3Connection(self._quic)
+
+    async def get(self, url: str, headers: Dict = {}) -> Deque[H3Event]:
+        """
+        Perform a GET request.
+        """
+        return await self._request(
+            HttpRequest(method="GET", url=URL(url), headers=headers)
+        )
+
+    def http_event_received(self, event: H3Event) -> None:
+        if isinstance(event, (HeadersReceived, DataReceived)):
+            stream_id = event.stream_id
+            if stream_id in self._request_events:
+                # http
+                self._request_events[event.stream_id].append(event)
+                if event.stream_ended:
+                    request_waiter = self._request_waiter.pop(stream_id)
+                    request_waiter.set_result(
+                        self._request_events.pop(stream_id))
+
+            elif event.push_id in self.pushes:
+                # push
+                self.pushes[event.push_id].append(event)
+
+        elif isinstance(event, PushPromiseReceived):
+            self.pushes[event.push_id] = deque()
+            self.pushes[event.push_id].append(event)
+
+    def quic_event_received(self, event: QuicEvent) -> None:
+        #  pass event to the HTTP layer
+        if self._http is not None:
+            for http_event in self._http.handle_event(event):
+                self.http_event_received(http_event)
+
+    async def _request(self, request: HttpRequest) -> Deque[H3Event]:
+        stream_id = self._quic.get_next_available_stream_id()
+        self._http.send_headers(
+            stream_id=stream_id,
+            headers=[
+                (b":method", request.method.encode()),
+                (b":scheme", request.url.scheme.encode()),
+                (b":authority", request.url.authority.encode()),
+                (b":path", request.url.full_path.encode()),
+                (b"user-agent", USER_AGENT.encode()),
+            ]
+            + [(k.encode(), v.encode()) for (k, v) in request.headers.items()],
+        )
+        self._http.send_data(stream_id=stream_id,
+                             data=request.content, end_stream=True)
+
+        waiter = self._loop.create_future()
+        self._request_events[stream_id] = deque()
+        self._request_waiter[stream_id] = waiter
+        self.transmit()
+
+        return await asyncio.shield(waiter)
+
+
+async def perform_http_request(
+    client: HttpClient,
+    url: str,
+    data: str,
+    include: bool
+) -> None:
+    # perform request
+    start = time.time()
+    http_events = await client.get(url)
+
+    # print speed
+    octets = 0
+    for http_event in http_events:
+        if isinstance(http_event, DataReceived):
+            octets += len(http_event.data)
+
+
+def process_http_pushes(
+    client: HttpClient,
+    include: bool,
+) -> None:
+    for _, http_events in client.pushes.items():
+        method = ""
+        octets = 0
+        path = ""
+        for http_event in http_events:
+            if isinstance(http_event, DataReceived):
+                octets += len(http_event.data)
+            elif isinstance(http_event, PushPromiseReceived):
+                for header, value in http_event.headers:
+                    if header == b":method":
+                        method = value.decode()
+                    elif header == b":path":
+                        path = value.decode()
+
+
+def save_session_ticket(ticket: SessionTicket) -> None:
+    """
+    Callback which is invoked by the TLS engine when a new session ticket
+    is received.
+    """
+    # if args.session_ticket:
+    #    with open(args.session_ticket, "wb") as fp:
+    #        pickle.dump(ticket, fp)
+
+
+def check_http3(url):
+    try:
+
+        print('A')
+
+        # prepare configuration
+        configuration = QuicConfiguration(
+            is_client=True, alpn_protocols=H3_ALPN,
+            verify_mode=ssl.CERT_NONE
+        )
+
+        print('B')
+
+        # parse URL
+        parsed = urlparse(url)
+        assert parsed.scheme in (
+            "https"
+        ), "Only https:// or wss:// URLs are supported."
+        host = parsed.hostname
+        print('C', host)
+        if parsed.port is not None:
+            print('D1')
+            port = parsed.port
+        else:
+            port = 443
+            print('D2', host, port)
+
+            with connect(
+                host,
+                port,
+                configuration=configuration,
+                create_protocol=HttpClient,
+                session_ticket_handler=save_session_ticket,
+                local_port=0,
+                wait_connected=False,
+            ) as client:
+                #    print('D2A')
+                client = cast(HttpClient, client)
+                #    print('D2B')
+
+                # perform request
+                coros = [
+                    perform_http_request(
+                        client=client,
+                        url=url,
+                        data=None,
+                        include=False
+                    )
+                ]
+                asyncio.gather(*coros)
+
+                # process http pushes
+                process_http_pushes(
+                    client=client, include=False)
+
+    except Exception as ex2:
+        print('EX:', ex2)
+        return (False, "http3")
 
 
 def check_http_fallback(url):
