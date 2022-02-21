d11f957e797cb2e3db11abd16c278c501435ccbe
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2022-02-21 20:14:06 +0100

    test changing directory

--- .github/workflows/verify_result.py
+++ .github/workflows/verify_result.py
@@ -16,6 +16,9 @@ def create_docker_steps():
     dir = os.path.dirname(os.path.realpath(__file__)) + os.path.sep
     files = os.listdir(dir)
     output = list('')
+    output.append('echo "the PWD is : ${pwd}"')
+    output.append('cd /webperf-core')
+    output.append('echo "the PWD is : ${pwd}"')
 
     cmd_history = set()
 
