ab8ac04202ef6cb5a78cd81c828e1a6402e7d4cc
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2022-02-23 21:57:22 +0100

    changed npm cmd

--- tests/frontend_quality_yellow_lab_tools.py
+++ tests/frontend_quality_yellow_lab_tools.py
@@ -77,7 +77,7 @@ def run_test(_, langCode, url, device='phone'):
     else:
         import subprocess
 
-        bashCommand = "npm-run yellowlabtools {0}".format(url)
+        bashCommand = "npm exec yellowlabtools {0}".format(url)
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
 
